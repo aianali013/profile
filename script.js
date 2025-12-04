edc02ce45df7f269dfb0b969394d95053cdfe989
aianali013 <aianasannar@gmail.com>
2025-12-04 14:34:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".edit-btn").onclick = function () {
+    alert("Edit button clicked!");
+};
+
+document.querySelector(".add-btn").onclick = function () {
+    alert("Add Email button clicked!");
+};
